d9f37a76cde1d8fe62e09904d22dba7192d37cb8
Thiago Banfi Credico <hbanfi3@hotmail.com>
2023-08-19 13:38:22 -0300

    feat: fetching w/ react server components (RSC)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,45 @@
-import Image from 'next/image'
+import { log } from 'console';
+import Image from 'next/image';
 
-export default function Home() {
+export type Country = {
+  name: {
+    common: string;
+  };
+
+  translations: {
+    por: {
+      common: string;
+    };
+  };
+
+  flags: {
+    svg: string;
+    alt: string;
+  };
+
+  capital: string;
+  region: string;
+  subregion: string;
+  population: number;
+  languages?: {
+    [key: string]: string;
+  };
+
+  borders?: string[];
+  cca3: string;
+};
+
+async function getCountries(): Promise<Country[]> {
+  const response = await fetch('https://restcountries.com/v3.1/all');
+  return response.json();
+}
+
+export default async function Home() {
+  const countries = await getCountries();
+  console.log(countries);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-HOME
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      HOME
     </main>
-  )
+  );
 }
